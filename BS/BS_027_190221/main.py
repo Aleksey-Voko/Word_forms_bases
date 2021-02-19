74bc5216e2c8c0910af5fe9d376ea78a95d2b264
Aleksey Voko <voko.aleksey@gmail.com>
2021-02-19 22:15:23 +0400

    Fixed BS_027_190221

--- BS/BS_027_190221/main.py
+++ BS/BS_027_190221/main.py
@@ -1,4 +1,6 @@
-from BS.utils import save_list_to_file, get_string_list_from_file, get_bs_title_word_form, get_socket_word_form
+from BS.utils import (save_list_to_file, get_string_list_from_file,
+                      get_bs_title_word_form, get_socket_word_form,
+                      read_src_bs)
 
 
 def get_replays_in_groups():
@@ -75,5 +77,33 @@ def get_remaining_homonyms():
     )
 
 
+def get_two_in_one():
+    remaining_repetitions = get_string_list_from_file('out/Повторы ост.txt')
+    word_forms_bases = read_src_bs('src_dict/БС 19.02.21.txt')
+    bs_word_forms = [x.title_word_form for x in word_forms_bases]
+
+    bs_word_names = [
+        ' '.join(filter(None, [
+            x.name,
+            x.idf,
+            ' '.join(x.info),
+            x.note
+        ]))
+        for x in bs_word_forms
+    ]
+
+    relevant = []
+    not_relevant = []
+
+    for repeat in remaining_repetitions:
+        if repeat in bs_word_names:
+            relevant.append(repeat)
+        else:
+            not_relevant.append(repeat)
+
+    save_list_to_file(relevant, 'out/Повторы ост. совпадает с БС.txt')
+    save_list_to_file(not_relevant, 'out/Повторы ост. не совпадает с БС.txt')
+
+
 if __name__ == '__main__':
-    get_remaining_repetitions()
+    get_two_in_one()
